8d0cc4fb04e37c5ffdbb2a281af3992b580c62b0
SlinkySlinky <slinkislinki@gmail.com>
2022-09-25 15:44:26 +0300

    Creating Bundle

---
+++ src/pages/index/index.js
@@ -0,0 +1,14 @@
+///// ***** Basic Plug-ins *****
+
+//Variables
+import '../../variables/var'
+//Vendors
+import '../../vendors/focus-visible'
+// Common Scripts
+import '../../components/_scripts/common.js'
+
+
+
+///// ***** Current Page Scripts *****
+
+
